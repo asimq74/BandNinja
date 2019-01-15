84bf3a330df9f39641b2c70d74678392f7837925
Asim Qureshi <aqureshi@geico.com>
2019-01-15 16:38:36 -0500

    fixed compiler error for activity_details

--- app/src/main/java/com/asimq/artists/bandninja/DetailsActivity.java
+++ app/src/main/java/com/asimq/artists/bandninja/DetailsActivity.java
@@ -1,6 +1,5 @@
 package com.asimq.artists.bandninja;
 
-import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
@@ -8,15 +7,10 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.ActivityOptionsCompat;
-import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.graphics.Palette;
-import android.support.v7.widget.Toolbar;
 import android.transition.Slide;
 import android.view.MotionEvent;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -29,9 +23,36 @@ public class DetailsActivity extends AppCompatActivity {
 	public static final String EXTRA_TITLE = "extraTitle";
 	private CollapsingToolbarLayout collapsingToolbarLayout;
 
+	private void applyPalette(Palette palette) {
+		int primaryDark = getResources().getColor(R.color.colorPrimaryDark);
+		int primary = getResources().getColor(R.color.colorPrimary);
+		collapsingToolbarLayout.setContentScrimColor(palette.getMutedColor(primary));
+		collapsingToolbarLayout.setStatusBarScrimColor(palette.getDarkMutedColor(primaryDark));
+		updateBackground((FloatingActionButton) findViewById(R.id.fab), palette);
+		supportStartPostponedEnterTransition();
+	}
+
+	@Override
+	public boolean dispatchTouchEvent(MotionEvent motionEvent) {
+		try {
+			return super.dispatchTouchEvent(motionEvent);
+		} catch (NullPointerException e) {
+			return false;
+		}
+	}
+
+	private void initActivityTransitions() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+			Slide transition = new Slide();
+			transition.excludeTarget(android.R.id.statusBarBackground, true);
+			getWindow().setEnterTransition(transition);
+			getWindow().setReturnTransition(transition);
+		}
+	}
 
 	@SuppressWarnings("ConstantConditions")
-	@Override protected void onCreate(Bundle savedInstanceState) {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		initActivityTransitions();
 		setContentView(R.layout.activity_details);
@@ -50,7 +71,13 @@ public class DetailsActivity extends AppCompatActivity {
 		final ImageView image = (ImageView) findViewById(R.id.image);
 		String extraImageUrl = getIntent().getStringExtra(EXTRA_IMAGE);
 		Picasso.with(this).load(extraImageUrl).into(image, new Callback() {
-			@Override public void onSuccess() {
+			@Override
+			public void onError() {
+
+			}
+
+			@Override
+			public void onSuccess() {
 				Bitmap bitmap = ((BitmapDrawable) image.getDrawable()).getBitmap();
 				Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
 					public void onGenerated(Palette palette) {
@@ -58,40 +85,10 @@ public class DetailsActivity extends AppCompatActivity {
 					}
 				});
 			}
-
-			@Override public void onError() {
-
-			}
 		});
 
 		TextView title = (TextView) findViewById(R.id.title);
 		title.setText(itemTitle);
-	}
-
-	@Override public boolean dispatchTouchEvent(MotionEvent motionEvent) {
-		try {
-			return super.dispatchTouchEvent(motionEvent);
-		} catch (NullPointerException e) {
-			return false;
-		}
-	}
-
-	private void initActivityTransitions() {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-			Slide transition = new Slide();
-			transition.excludeTarget(android.R.id.statusBarBackground, true);
-			getWindow().setEnterTransition(transition);
-			getWindow().setReturnTransition(transition);
-		}
-	}
-
-	private void applyPalette(Palette palette) {
-		int primaryDark = getResources().getColor(R.color.colorPrimaryDark);
-		int primary = getResources().getColor(R.color.colorPrimary);
-		collapsingToolbarLayout.setContentScrimColor(palette.getMutedColor(primary));
-		collapsingToolbarLayout.setStatusBarScrimColor(palette.getDarkMutedColor(primaryDark));
-		updateBackground((FloatingActionButton) findViewById(R.id.fab), palette);
-		supportStartPostponedEnterTransition();
 	}
 
 	private void updateBackground(FloatingActionButton fab, Palette palette) {
