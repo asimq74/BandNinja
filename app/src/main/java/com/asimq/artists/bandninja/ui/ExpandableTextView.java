cf3980b171809316d8025708488618a04c3ae7b6
Asim Qureshi <aqureshi@geico.com>
2019-02-18 17:10:21 -0500

    Implemented search view in navigation drawer

--- app/src/main/java/com/asimq/artists/bandninja/ui/ExpandableTextView.java
+++ app/src/main/java/com/asimq/artists/bandninja/ui/ExpandableTextView.java
@@ -2,12 +2,17 @@ package com.asimq.artists.bandninja.ui;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.support.v7.widget.AppCompatTextView;
 import android.text.SpannableStringBuilder;
+import android.text.TextPaint;
+import android.text.style.ClickableSpan;
 import android.util.AttributeSet;
 import android.view.View;
 
 import com.asimq.artists.bandninja.R;
+import com.asimq.artists.bandninja.utils.Util;
+import com.asimq.artists.bandninja.utils.Util.MySpannable;
 
 public class ExpandableTextView extends AppCompatTextView {
 	private static final int DEFAULT_TRIM_LENGTH = 200;
@@ -50,7 +55,7 @@ public class ExpandableTextView extends AppCompatTextView {
 
 	@Override
 	public void setText(CharSequence text, BufferType type) {
-		originalText = text;
+		originalText = text.toString().trim();
 		trimmedText = getTrimmedText(text);
 		bufferType = type;
 		setText();
@@ -58,8 +63,23 @@ public class ExpandableTextView extends AppCompatTextView {
 
 	private CharSequence getTrimmedText(CharSequence text) {
 		if (originalText != null && originalText.length() > trimLength) {
-			return new SpannableStringBuilder(originalText, 0, trimLength + 1)
-					.append(ELLIPSIS).append("\n").append(getContext().getResources().getString(R.string.clickForMore));
+			final String spannableText = getContext().getResources().getString(R.string.clickForMore);
+			final SpannableStringBuilder thisCharacterSequence = new SpannableStringBuilder(originalText, 0, trimLength + 1)
+					.append(ELLIPSIS).append("\n").append(spannableText);
+			SpannableStringBuilder ssb = new SpannableStringBuilder(thisCharacterSequence);
+
+			final String characterSequenceString = thisCharacterSequence.toString();
+			if (characterSequenceString.contains(spannableText)) {
+
+				ssb.setSpan(new Util.MySpannable(false) {
+					@Override
+					public void onClick(View widget) {
+					}
+				}, characterSequenceString.indexOf(spannableText), characterSequenceString.indexOf(spannableText)
+						+ spannableText.length(), 0);
+
+			}
+			return ssb;
 		} else {
 			return originalText;
 		}
@@ -73,6 +93,29 @@ public class ExpandableTextView extends AppCompatTextView {
 		this.trimLength = trimLength;
 		trimmedText = getTrimmedText(originalText);
 		setText();
+	}
+
+	public static class MySpannable extends ClickableSpan {
+
+		private boolean isUnderline = true;
+
+		/**
+		 * Constructor
+		 */
+		public MySpannable(boolean isUnderline) {
+			this.isUnderline = isUnderline;
+		}
+
+		@Override
+		public void onClick(View widget) {
+
+		}
+
+		@Override
+		public void updateDrawState(TextPaint ds) {
+			ds.setUnderlineText(isUnderline);
+			ds.setColor(Color.parseColor("#1b76d3"));
+		}
 	}
 
 	public int getTrimLength() {
